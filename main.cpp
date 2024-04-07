132c7ac2cf34a755b487defbdbc109f0658955dc
mohrez <mohrez2005namira730@gmail.com>
2024-04-08 00:22:49 +0330

    main1

--- main.cpp
+++ main.cpp
@@ -7,13 +7,10 @@
 
 int main()
 {
-    string name;
-    int age;
+    string name, namee;  int choice;
     char y;
     cout << "enter your name: ";
     getline(cin , name);
-    cout << "enter your age: ";
-    cin >> age;
     setConsoleColor(1);
     cout << "\nWELLCOME TO ";
     setConsoleColor(2);
@@ -21,21 +18,39 @@ int main()
     setConsoleColor(1);
     cout << "GAME " << name << "\n\n";
     setConsoleColor(15);
-    while(true){
+    
     cout << "1.existed" << "\n2.customize\n" << "(press \"1\" for choosing amoung existed characters and for customizing press \"2\")";
-    y =_getch();
+    cin >> y;
     characters ch;
     if (y=='1')
     {
-        ch.displaychars();
-        cin.get();
+        ch.choice();
+        
+        
     } else if (y=='2'){
-        cin.get();
+        
+        cout << "choose a name for your character:\n";
+        getline(cin ,namee);
+        cout << "how strong you like to be?!\n" << "1.noob\n2.normal\n3.pro\n";
+        cin >> choice;
+        if (choice==1)
+        {
+            
+        } else if (choice==2)
+        {
+            /* code */
+        } else
+        {
+            /* code */
+        }
+        
+        
+        
+
     } else {
         cout << "unvalid choice\n";
-        break;
     }
     }
     
-}
+
    
