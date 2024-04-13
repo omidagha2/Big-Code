0840d3052274c31c9ec2b7339e44c2462b431593
mohrez <mohrez2005namira730@gmail.com>
2024-04-13 22:53:02 +0330

    mainm

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
-#include <string>
-#include <vector>
 #include <iostream>
-#include <windows.h>
-#include <conio.h>
 #include "1m.cpp"
 
 int main()
@@ -19,38 +15,65 @@ int main()
     cout << "GAME " << name << "\n\n";
     setConsoleColor(15);
     
-    cout << "1.existed" << "\n2.customize\n" << "(press \"1\" for choosing amoung existed characters and for customizing press \"2\")";
-    cin >> y;
-    characters ch;
-    if (y=='1')
-    {
-        ch.choice();
-        
-        
-    } else if (y=='2'){
-        
-        cout << "choose a name for your character:\n";
-        getline(cin ,namee);
-        cout << "how strong you like to be?!\n" << "1.noob\n2.normal\n3.pro\n";
-        cin >> choice;
-        if (choice==1)
-        {
+    Game startGame;
+}
+    // cout << "1.existed" << "\n2.customize\n" << "(press \"1\" for choosing amoung existed characters and for customizing press \"2\")";
+    // cin >> y;
+    // characters ch;
+    // if (y=='1')
+    // {
+    //     human* selectedCharacter = ch.choice();
+    //     if (selectedCharacter) {
+    //         StoryHandler story;
+    //         while (true)
+    //         {
+    //             story.displayStory();
+    //         }
             
-        } else if (choice==2)
-        {
-            /* code */
-        } else
-        {
-            /* code */
-        }
+    //     }
+        
+    // } else if (y=='2'){
+    //     string gender;
+    //     cout << "choose a name for your character:\n";
+    //     getline(cin ,namee);
+    //     cout << "\n1.Male\n2.Female\n";
+    //     cin >> gender;
+    //     cout << "how strong you like to be?!\n" << "1.noob\n2.normal\n3.pro\n";
+    //     cin >> choice;
+    //     if (choice==1)
+    //     {
+    //         human player(namee,10 ,gender ,1 ,20 ,10 ,20);
+
+    //     } else if (choice==2)
+    //     {
+    //         /* code */
+    //     } else
+    //     {
+    //         /* code */
+    //     }
         
         
         
 
-    } else {
-        cout << "unvalid choice\n";
-    }
-    }
+    // } else {
+    //     cout << "unvalid choice\n";
+    // }
+    //}
+//     int main() {
+//     GameModel model;
+//     GameView view;
+//     GameController controller;
+
+//     // Game loop
+//     while (true) {
+//         view.displayGameState(model);
+//         controller.handleInput(model);
+//         // Update the game state
+//     }
+
+//     return 0;
+// }
+
     
 
    
